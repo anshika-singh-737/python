7daf385150079d42b57d215c457e6da3c6a2b37f
Anshika Singh <anshikaaaa0787@gmail.com>
2026-08-23 01:04:56 +0530

    Python

---
+++ quizconditional.py
@@ -0,0 +1,10 @@
+a= int(input("enter your age"))
+
+if a >= 18: #if age is 18 or older
+    print ("yes")
+
+elif a < 18: # if age is under 18
+    print("n0")
+
+else:      #otherwise
+    print("maybe")
