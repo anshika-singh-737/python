ddbae52d0d1540113129568bce76aacbdc55814e
Anshika Singh <anshikaaaa0787@gmail.com>
2026-08-23 00:58:44 +0530

    Python

---
+++ conditionalexp.py
@@ -0,0 +1,17 @@
+a = int(input("enter your age;"))
+#if elif else ladder
+
+if(a>=18):
+    print("you are above the age of consent")
+
+elif(a<0):
+    print("you are entering invalid -ive age")
+elif(a==0):
+    print("you are entering invalid age")
+
+else:
+    print("you are below the age of consent")
+
+
+print("end of your program")
+
